8a6193cf87d9b8240d31329ff3ef5392ebbb9f32
Fiberx01 <75884799+fiberx10@users.noreply.github.com>
2023-10-06 15:15:23 +0100

    base:app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,169 @@
-import Image from 'next/image'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+
+
+
+const styles = {
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "12px 32px",
+    borderRadius: "4px",
+    backgroundColor: "green",
+    margin: "10px",
+    width: "220px",
+    textAlign: "center",
+  },
+  tab: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "12px 32px",
+    backgroundColor: "white",
+    width: "100%",
+    height: "60px",
+    color: "gray",
+  },
+  tabActive: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "12px 32px",
+    backgroundColor: "white",
+    width: "100%",
+    height: "60px",
+    color: "green",
+    borderBottom: "2px solid green",
+  },
+  text: {
+    fontSize: "16px",
+    lineHeight: "21px",
+    fontWeight: "bold",
+    letterSpacing: "0.25px",
+    color: "white",
+  },
+};
+
+function Tab(props: { onClick: any; title?: any; active: any; disabled: any }) {
+  const { onClick, title = "Save", active, disabled } = props;
+  return (
+    <button
+      disabled={disabled}
+      style={active ? styles.tabActive : styles.tab}
+      onClick={onClick}
+    >
+      <span style={{ ...styles.text, color: active ? "green" : "gray" }}>
+        {title}
+      </span>
+    </button>
+  );
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [tab, setTab] = useState("S1");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+        height: "100vh",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "row"  , width: "100%" , maxWidth: "500px"}}>
+        <Tab
+          title="Semestre 1"
+          active={tab == "S1"}
+          onClick={() => setTab("S1")}
+          disabled={undefined}
+        />
+        <Tab
+          title="Semestre 2"
+          active={tab == "S2"}
+          disabled
+          onClick={() => setTab("S2")}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "20px",
+        }}
+      >
+        <h1
+          style={{
+            fontWeight: "bold",
+            color: "black",
+            fontSize: "25px",
+            textAlign: "center",
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Bonjour, bienvenue sur l'application de gestion des emplois du temps
+          d'
+          <span style={{ color: "green" }}>
+            ESI<span style={{ color: "red" }}>2</span>A
+          </span>
+        </h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p style={{ color: "grey", fontSize: "18px", margin: "30px" }}>
+          veuillez choisir votre niveau
+        </p>
+        <div
+          style={{
+            display: "grid",
+            flexDirection: "column",
+            placeItems: "center",
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <Link
+            href={"/time/?semester=" + tab + "&level=CPI1"}
+            style={{
+              ...styles.button,
+              backgroundColor: "green",
+            }}
+          >
+            <span style={styles.text}>{"CPI1 - " + tab}</span>
+          </Link>
+          <Link
+            href={"/time/?semester=" + tab + "&level=CPI2"}
+            style={{
+              ...styles.button,
+              backgroundColor: "green",
+            }}
+          >
+            <span style={styles.text}>{"CPI2 - " + tab}</span>
+          </Link>
+          <Link
+            href={"/time/?semester=" + tab + "&level=CI1"}
+            style={{
+              ...styles.button,
+              backgroundColor: "green",
+            }}
+          >
+            <span style={styles.text}>{"CI1 - " + tab}</span>
+          </Link>
+          <Link
+            href={"/time/?semester=" + tab + "&level=CI2"}
+            style={{
+              ...styles.button,
+              backgroundColor: "green",
+            }}
+          >
+            <span style={styles.text}>{"CI2 - " + tab}</span>
+          </Link>
+          <Link
+            href={"/time/?semester=" + tab + "&level=CI3"}
+            style={{
+              ...styles.button,
+              backgroundColor: "green",
+            }}
+          >
+            <span style={styles.text}>{"CI3 - " + tab}</span>
+          </Link>
+        </div>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
---
+++ src/app/time/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useRouter } from "next/router";
+import { useSearchParams } from 'next/navigation'
+
+
+const timeTablesLinks = {
+    S1: {
+      CPI1: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQBUTtIzZTrSBIojkuA10r3Yt_2KusW2lIIe-z_HIi2k-guTUx3cPXxH4a5-BXUszaxgfAQWYqTXsvd/pubhtml",
+      CPI2: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQnagUH8Z_T5SrJJBgaIU91tuYDd_BM05uYMjGgKhgnOtYsXZekH978GZVkregbe9ry6i7uLsLcMjH3/pubhtml",
+      CI1: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRP-HQUocyuqN3kL7lV8kRSDdSMWiCleHoUNtwflwYoNAw3uhIY8m8xxjVjW08DfuOHpe5vr_a8wHu_/pubhtml",
+      CI2: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSOvm4s-hP22n2X_XQtMfO2F5bSWrzybL-DKGYxLYU6pufy1oVqpNNcsa5X8g2ClgDvT9GwHuR6pwiG/pubhtml",
+      CI3: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCwqQ7hG4MPz65AFPc3KT8Xph3Yf1InmPc5XzooTejA0XzsJ9SmE6t9cOIyuQMPm9b6x2m73ETeB0p/pubhtml",
+    },
+    S2: {
+      CPI1: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSS3W_p7bAz4EmeuafOuVPLiO-Gtp6-WAfai_eR1EkwUvmiKyrENfH3dcPfggdevnUH1xpjhWIURxyz/pubhtml",
+      CPI2: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTBLMHqCiJocbMJz4M3Cm0ASuBN4znBUi08TBBOraKVEMJfb4e1EaVPSG3DWF2LIxwNhSrHolZetdBt/pubhtml",
+      CI1: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTjZVrj90pkm3wLRemYXsMkOGLRqE-2QZ15Eu94SVPGSzNrfYVg-neGYx7lr_kMchWFAOutkUqZYHmJ/pubhtml",
+      CI2: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRU0vk03KvvDrHr5YNx_iSunwS5N6rVwjJtVQ-SlmhxkpT1z1gtpF6l8R4SB1xFMAS_C61D-SOyUUWO/pubhtml",
+    },
+  };
+
+
+
+function TimeTable() {
+    const searchParams = useSearchParams()
+    const semester = searchParams.get("semester") || "S1"
+    const level = searchParams.get("level") || "CPI1"
+    return (
+    <iframe src={
+        timeTablesLinks[semester as string][level]
+    } style={{ width: "100vw", height: "100vh" }}></iframe>
+  );
+}
+
+export default TimeTable;
